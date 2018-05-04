cdfc097a09dafa48b350fe2c964af40fa52068a7
elmariocarlos <elmariocarlos@users.noreply.github.com>
2018-05-04 03:51:53 -0500

    Testing

---
+++ helloworld.js
@@ -0,0 +1,53 @@
+//Esta madre no está jalando
+
+/*Y ando probando
+bastante
+este pedo
+*/
+
+var PRECIO = 200.01;
+
+var NOMBRE = "Mario 'El KwAmOn'";
+
+document.write("<h1>Hello World</h1>");
+
+document.write(PRECIO);
+
+document.write(NOMBRE);
+
+PRECIO--;
+
+document.write(PRECIO);
+
+PRECIO += 10;
+
+document.write(PRECIO);
+
+PRECIO = 30;
+
+if (PRECIO > 50) {
+	document.write("Hola mundo again");
+}
+else if (PRECIO > 100){
+	document.write(PRECIO <= 50);
+	document.write(PRECIO > 50 && PRECIO>=0);
+}
+else {
+	document.write(NOMBRE+"WOWOWOWOWOW");
+}
+
+var day = 5;
+switch (day) {
+    case 1:
+        document.write("Monday");
+        break;
+    case 2:
+        document.write("Tuesday");
+        break;
+    case 3:
+        document.write("Wednesday");
+        break;
+    default:
+        document.write("Another day");
+}
+
